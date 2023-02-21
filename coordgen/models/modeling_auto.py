43aac73f7c33965ee00265327a182d4d4c308468
Hiroki Teranishi <hiroki.teranishi@riken.jp>
2023-02-21 15:31:47 +0900

    Reimplement AutoModel.

--- coordgen/models/modeling_auto.py
+++ coordgen/models/modeling_auto.py
@@ -1,55 +1,91 @@
 import importlib
+from types import ModuleType
+from typing import Any, Dict
 
-from transformers.models.auto import auto_factory, configuration_auto, tokenization_auto
+from transformers import AutoConfig, AutoTokenizer, PretrainedConfig
 
 
-class _LazyAutoMapping(auto_factory._LazyAutoMapping):
-    def _load_attr_from_module(self, model_type, attr):
-        if "." in attr:
-            module_name, attr = attr.rsplit(".", 1)
-        else:
-            module_name = configuration_auto.model_type_to_module_name(model_type)
-            module_name = f"transformers.models.{module_name}"
+class _LazyAutoMapping(Dict[str, Any]):
+    def __init__(self, mapping: Dict[str, str]):
+        super().__init__()
+        self._mapping = mapping
+        self._modules: Dict[str, ModuleType] = {}
+
+    def __missing__(self, key: str) -> Any:
+        if key not in self._mapping:
+            raise KeyError(key)
+
+        module_name, attr = self._mapping[key].rsplit(".", 1)
         if module_name not in self._modules:
             self._modules[module_name] = importlib.import_module(module_name)
-        return getattr(self._modules[module_name], attr)
+
+        value = getattr(self._modules[module_name], attr)
+        return self.setdefault(key, value)
+
+
+class _BaseAutoModelClass:
+    _model_mapping: _LazyAutoMapping
+
+    def __init__(self, *args, **kwargs):
+        raise EnvironmentError(
+            f"{self.__class__.__name__} is designed to be instantiated using the "
+            f"`{self.__class__.__name__}.from_pretrained(pretrained_model_name_or_path)` method."
+        )
+
+    @classmethod
+    def _get_model_class(cls, config: PretrainedConfig):
+        try:
+            return cls._model_mapping[type(config).__name__]
+        except KeyError:
+            raise ValueError(
+                "Unrecognized configuration class {} for this kind of AutoModel: {}.\n"
+                "Model type should be one of {}.".format(
+                    config.__class__,
+                    cls.__name__,
+                    ", ".join(k for k in cls._model_mapping._mapping.keys()),
+                )
+            ) from None
 
 
 MODEL_MAPPING = _LazyAutoMapping(
-    configuration_auto.CONFIG_MAPPING_NAMES,
     {
-        "bert": "transformers.models.bert.BertForMaskedLM",
-        "t5": "coordgen.models.modeling_t5.T5ForConditionalGeneration",
+        "BertConfig": "transformers.models.bert.BertForMaskedLM",
+        "T5Config": "coordgen.models.modeling_t5.T5ForConditionalGeneration",
     },
 )
 
 MODEL_FOR_COORDINATION_GENERATION_MAPPING = _LazyAutoMapping(
-    configuration_auto.CONFIG_MAPPING_NAMES,
     {
-        "bert": "coordgen.models.modeling_bert.BertForCoordinationGeneration",
-        "t5": "coordgen.models.modeling_t5.T5ForCoordinationGeneration",
+        "BertConfig": "coordgen.models.modeling_bert.BertForCoordinationGeneration",
+        "T5Config": "coordgen.models.modeling_t5.T5ForCoordinationGeneration",
     },
 )
 
 
-class _AutoModel(auto_factory._BaseAutoModelClass):
+class _AutoModel(_BaseAutoModelClass):
     _model_mapping = MODEL_MAPPING
 
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
+        config = kwargs.pop("config", None)
+        if not isinstance(config, PretrainedConfig):
+            config, kwargs = AutoConfig.from_pretrained(
+                pretrained_model_name_or_path,
+                return_unused_kwargs=True,
+                **kwargs,
+            )
+        return cls._get_model_class(config).from_pretrained(
+            pretrained_model_name_or_path, *model_args, config=config, **kwargs
+        )
 
-class AutoModelForCoordinationGeneration:
+
+class AutoModelForCoordinationGeneration(_BaseAutoModelClass):
     _model_mapping = MODEL_FOR_COORDINATION_GENERATION_MAPPING
     _auto_model_class = _AutoModel
 
-    def __init__(self, *args, **kwargs):
-        class_name = self.__class__.__name__
-        raise EnvironmentError(
-            f"{class_name} is designed to be instantiated "
-            f"using the `{class_name}.from_pretrained(pretrained_model_name_or_path)` method."
-        )
-
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
-        tokenizer = tokenization_auto.AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+        tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
         model = cls._auto_model_class.from_pretrained(pretrained_model_name_or_path)
-        model_class = cls._model_mapping[type(model.config)]
+        model_class = cls._get_model_class(model.config)
         return model_class(model, tokenizer, **kwargs)
