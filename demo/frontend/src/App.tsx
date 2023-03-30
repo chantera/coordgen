4771f8d178eee1c1d8bf5ccb279d40091bd39a4d
Hiroki Teranishi <hiroki.teranishi@riken.jp>
2023-03-30 11:23:56 +0900

    Update client.

--- demo/frontend/src/App.tsx
+++ demo/frontend/src/App.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-const Selector = () => {
+interface Coord {
+  text: string;
+  cc: [number, number];
+  conjuncts: [number, number][];
+}
+
+const Selector = (props: any) => {
   const [input, setInput] = useState<string>("");
   const [span, setSpan] = useState<[number, number] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const setText = (v: string) => {
-    setInput(v);
-    setSpan(null);
-  }
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(input);
-    console.log(span);
-  }
-
-  const handleSelect = (e: any) => {
+  const handleSelect = (e: React.SyntheticEvent<HTMLTextAreaElement>) => {
+    if (!(e.target instanceof HTMLTextAreaElement)) {
+      return;
+    }
     const start = e.target.selectionStart;
     const end = e.target.selectionEnd;
     if (end > start) {
@@ -24,8 +24,42 @@ const Selector = () => {
     }
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // console.log(input);
+    // console.log(span);
+    e.preventDefault();
+    if (!span || loading) {
+      return;
+    }
+    setLoading(true);
+    axios
+      .post("http://localhost:8000/generate", {
+        text: input, start: span[0], end: span[1]
+      })
+      .then((res) => {
+        const coord: Coord = {
+          text: res.data.text,
+          cc: [res.data.cc[0], res.data.cc[1]],
+          conjuncts: res.data.conjuncts.map((x: any) => [x.start, x.end])
+        };
+        props.append(coord);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Oops! Something went wrong.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  const setText = (v: string) => {
+    setInput(v);
+    setSpan(null);
+  }
+
   return (
-    <form onSubmit={handleSubmit} className="bg-white rounded py-6 px-4 shadow">
+    <form onSubmit={handleSubmit} className="bg-white rounded py-6 px-4 mb-1 shadow">
       <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-4 gap-x-8">
         <div className="sm:col-span-4">
           <label htmlFor="input" className="block label">Input</label>
@@ -37,10 +71,10 @@ const Selector = () => {
           <code className="block h-10 preview truncate">{span && `(${span[0]}, ${span[1]}): ${input.substring(span[0], span[1])}`}</code>
         </div>
         <div>
-          <button type="submit" className="block w-full btn btn-primary">Generate</button>
+          <button type="submit" disabled={!span || loading} className="block w-full btn btn-primary">Generate</button>
         </div>
         <div>
-          <button type="reset" onClick={() => setText("")} className="block w-full btn">Reset</button>
+          <button type="reset" onClick={() => { setText(""); props.clear(); }} className="block w-full btn">Reset</button>
         </div>
       </div>
     </form>
@@ -48,6 +82,16 @@ const Selector = () => {
 }
 
 const App = () => {
+  const [coords, setCoords] = useState<Coord[]>([]);
+
+  const results = coords.map((coord, i) => {
+    return (
+      <li key={i}>
+        <p>{coord.text}</p>
+      </li>
+    );
+  });
+
   return (
     <div className="min-h-full">
       <header className="bg-white shadow-sm">
@@ -57,7 +101,11 @@ const App = () => {
       </header>
       <main>
         <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
-          <Selector />
+          <Selector
+            append={(coord: Coord) => setCoords(coords.concat([coord]))}
+            clear={() => setCoords([])}
+          />
+          <ol>{results}</ol>
         </div>
       </main>
     </div>
